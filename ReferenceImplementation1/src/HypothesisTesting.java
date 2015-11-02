5ed76c5defc8cc2f34ba46a7a26f0aee84856205
Kyle-Doud <krdoud@gmail.com>
2015-11-02 17:11:14 -0600

    LTL Conversion
    Need to find postfixes and replace Q, R, S, T, etc properly in the
    formulas.
    Change path of Patterns.xml in HypothesisTesting.

--- ReferenceImplementation1/src/HypothesisTesting.java
+++ ReferenceImplementation1/src/HypothesisTesting.java
@@ -1,15 +1,38 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class HypothesisTesting {
 	String hypothesisName;
-	public enum Patterns { Precedence, Existence , Universality , Absence , Response , Custom};
-	Patterns specificationPattern;
-	Query q;
+	Query query;
+	Document dom;
 	
-	public HypothesisTesting(String name, Patterns p, Query q ){
+	public HypothesisTesting(String name, Query q )
+	{
 		this.hypothesisName = name;
-		this.q = q;
-		this.specificationPattern = p;
-		
+		this.query = q;
+	}
+	
+	public String toLTL()
+	{
+		parseXmlFile();
+		String ltlFormula = getFormulaFromXml();
+		String[] placeholders = {"P", "S", "R", "X", "Z"};
+		for(int i = 0; i < query.getEvents().size(); i++)//problem: if the text we replace has a capital P, S, etc in it, we'll replace stuff we shouldn't
+		{
+			ltlFormula = ltlFormula.replaceAll(placeholders[i], query.getEvents().get(i));
+		}
+		return ltlFormula;
 	}
 
 	public String getHypothesisName() {
@@ -20,20 +43,69 @@ public class HypothesisTesting {
 		this.hypothesisName = hypothesisName;
 	}
 
-	public Patterns getSpecificationPattern() {
-		return specificationPattern;
+//	public Patterns getSpecificationPattern() {
+//		return specificationPattern;
+//	}
+//
+//	public void setSpecificationPattern(Patterns specificationPattern) {
+//		this.specificationPattern = specificationPattern;
+//	}
+
+	public Query getQuery() {
+		return query;
 	}
 
-	public void setSpecificationPattern(Patterns specificationPattern) {
-		this.specificationPattern = specificationPattern;
+	public void setQuery(Query q) {
+		this.query = q;
 	}
+	
+	private void parseXmlFile(){
+		//get the factory
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		
+		try {
+			
+			//Using factory get an instance of document builder
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			
+			//parse using builder to get DOM representation of the XML file
+			dom = db.parse("C:\\Users\\krdou_000\\Documents\\Repast Workspace\\LTLtoXML\\Patterns.xml");
+			
 
-	public Query getQ() {
-		return q;
+		}catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}catch(SAXException se) {
+			se.printStackTrace();
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
 	}
+	
+	private String getFormulaFromXml()
+	{
+		String formula = "not identified";
+		Element docEle = dom.getDocumentElement();
+		
+		NodeList nl = docEle.getElementsByTagName(query.getPatternType());
+		if(nl != null && nl.getLength() > 0) {
+			for(int i = 0 ; i < nl.getLength();i++) 
+			{
+				Element el = (Element)nl.item(i);
+				formula = getTextValue(el, "Globally");
+			}
+		}
+		return formula;
+	}
+	
+	private String getTextValue(Element ele, String tagName) {
+		String textVal = null;
+		NodeList nl = ele.getElementsByTagName(tagName);
+		if(nl != null && nl.getLength() > 0) {
+			Element el = (Element)nl.item(0);
+			textVal = el.getFirstChild().getNodeValue();
+		}
 
-	public void setQ(Query q) {
-		this.q = q;
+		return textVal;
 	}
 
 }
--- ReferenceImplementation1/src/Query.java
+++ ReferenceImplementation1/src/Query.java
@@ -1,78 +1,174 @@
+import java.util.ArrayList;
+
 
 public class Query {
 
-	Event e;
-	public Event getE() {
-		return e;
-	}
+	private ArrayList<String> events = new ArrayList<String>();
+	private String query;
+	private String patternType;
 
-	public void setE(Event e) {
-		this.e = e;
-	}
+	public final String[] patternIdentifier = {"absent", "eventually exists", "always exists", "precedes", "leads to"};
+	public final String[] patterns = {"Absence", "Existence", "Universality", "Precedence", "Response"};
+	//public enum patternsEnum {absence, existence, universality, precedence, response};
 
-	public temp getTemporalLogic() {
-		return temporalLogic;
-	}
+	//public enum log {and, or, not};
+	//public enum linkers {is, occurs, to};
+	//public enum Exp {TRUE, FALSE}
+	//public String[] tempDefs = {"precedes", "between", "eventually", "always",  "before", "until", "never", "leads", "Exists"};
+	//public enum temp {precedes, between, eventually, always,  before, until, never, leads, Exists};
+	//	temp temporalLogic;
+	//	log Logical;
+	//	linkers Link;
+	//	Exp expression;
 
-	public void setTemporalLogic(temp temporalLogic) {
-		this.temporalLogic = temporalLogic;
-	}
 
-	public log getLogical() {
-		return Logical;
-	}
-
-	public void setLogical(log logical) {
-		Logical = logical;
-	}
-
-	public linkers getLink() {
-		return Link;
-	}
-
-	public void setLink(linkers link) {
-		Link = link;
-	}
-
-	public Exp getExpression() {
-		return expression;
-	}
-
-	public void setExpression(Exp expression) {
-		this.expression = expression;
-	}
-
-	public enum temp {precedes, between, eventually, always,  before, until, never, leads, Exists}
-	public enum log {and, or, not};
-	public enum linkers {is, occurs, to};
-	public enum Exp {TRUE, FALSE}
-	
-	temp temporalLogic;
-	log Logical;
-	linkers Link;
-	Exp expression;
-	
-	public Query(Event e, temp t, linkers link, Exp exp){
-		this.e =e;
-		this.temporalLogic =t;
-		
-		this.Link =link;
-		this.expression = exp;
+	/**
+	 * Creates a new query object.
+	 * 
+	 * @param queryIn The query from the DSL as a string. For example, "LookingForNewSite = true is absent"
+	 */
+	public Query(String queryIn)
+	{
+		this.query = queryIn;
+		detectPattern();
+		detectEvents();
 	}
 	
-	public Query(Event e, log l, linkers link, Exp exp){
-		this.e =e;
-		
-		this.Logical =l;
-		this.Link =link;
-		this.expression = exp;
+	/**
+	 * Helper function that detects the type of pattern the query is by looking for keywords.
+	 */
+	private void detectPattern()
+	{
+		for(int i = 0; i < patternIdentifier.length; i++)
+		{
+			if(query.contains(patternIdentifier[i]))
+			{
+				patternType = patterns[i];
+			}
+		}
 	}
 	
-	public Query(Event e, temp t, log l, linkers link, Exp exp){
-		this.e =e;
-		this.temporalLogic =t;
-		this.Logical =l;
-		this.Link =link;
-		this.expression = exp;
+	/**
+	 * Helper function that detects the events in the query by searching before and after the pattern keywords
+	 */
+	private void detectEvents()
+	{
+		switch(patternType)
+		{
+		case "Absence":
+			events.add(query.substring(0, query.indexOf("is") - 1));
+			break;
+		case "Existence":
+			events.add(query.substring(0, query.indexOf("eventually") - 1));
+			break;
+		case "Universality":
+			events.add(query.substring(0, query.indexOf("always") - 1));
+			break;
+		case "Precedence":
+			events.add(query.substring(0, query.indexOf("precedes") - 1));
+			events.add(query.substring(query.indexOf("precedes")+ 9));
+			break;
+		case "Response":
+			events.add(query.substring(0, query.indexOf("leads") - 1));
+			events.add(query.substring(query.indexOf("to") + 3));
+			break;
+		default:
+			break;
+
+		}
 	}
+	
+	/**
+	 * Gets the pattern type
+	 * 
+	 * @return patternType The name of the specification pattern used in the query
+	 */
+	public String getPatternType() {
+		return patternType;
+	}
+
+	/**
+	 * Sets the patterns type
+	 * 
+	 * @param patternType The name of the new specification pattern to be used in the query
+	 */
+	public void setPatternType(String patternType) {
+		this.patternType = patternType;
+	}
+
+	/**
+	 * Gets the list of events
+	 * 
+	 * @return patternType The name of the specification pattern used in the query
+	 */
+	public ArrayList<String> getEvents() {
+		return events;
+	}
+
+	/**
+	 * Sets the patterns type
+	 * 
+	 * @param patternType The name of the new specification pattern to be used in the query
+	 */
+	public void setEvents(ArrayList<String> e) {
+		this.events = e;
+	}
+
+//	public temp getTemporalLogic() {
+//		return temporalLogic;
+//	}
+//
+//	public void setTemporalLogic(temp temporalLogic) {
+//		this.temporalLogic = temporalLogic;
+//	}
+//
+//	public log getLogical() {
+//		return Logical;
+//	}
+//
+//	public void setLogical(log logical) {
+//		Logical = logical;
+//	}
+//
+//	public linkers getLink() {
+//		return Link;
+//	}
+//
+//	public void setLink(linkers link) {
+//		Link = link;
+//	}
+//
+//	public Exp getExpression() {
+//		return expression;
+//	}
+//
+//	public void setExpression(Exp expression) {
+//		this.expression = expression;
+//	}
+
+
+
+	//	public Query(Event e, temp t, linkers link, Exp exp){
+	//		this.e =e;
+	//		this.temporalLogic =t;
+	//		
+	//		this.Link =link;
+	//		this.expression = exp;
+	//	}
+	//	
+	//	public Query(Event e, log l, linkers link, Exp exp){
+	//		this.e =e;
+	//		
+	//		this.Logical =l;
+	//		this.Link =link;
+	//		this.expression = exp;
+	//	}
+	//	
+	//	public Query(Event e, temp t, log l, linkers link, Exp exp){
+	//		this.e =e;
+	//		this.temporalLogic =t;
+	//		this.Logical =l;
+	//		this.Link =link;
+	//		this.expression = exp;
+	//	}
 }
