b162caaf582c20646bbc3ed45402470da85ccc1c
Kyle-Doud <krdoud@gmail.com>
2015-11-02 17:15:11 -0600

    Removed dead code

--- ReferenceImplementation1/src/Query.java
+++ ReferenceImplementation1/src/Query.java
@@ -9,18 +9,6 @@ public class Query {
 
 	public final String[] patternIdentifier = {"absent", "eventually exists", "always exists", "precedes", "leads to"};
 	public final String[] patterns = {"Absence", "Existence", "Universality", "Precedence", "Response"};
-	//public enum patternsEnum {absence, existence, universality, precedence, response};
-
-	//public enum log {and, or, not};
-	//public enum linkers {is, occurs, to};
-	//public enum Exp {TRUE, FALSE}
-	//public String[] tempDefs = {"precedes", "between", "eventually", "always",  "before", "until", "never", "leads", "Exists"};
-	//public enum temp {precedes, between, eventually, always,  before, until, never, leads, Exists};
-	//	temp temporalLogic;
-	//	log Logical;
-	//	linkers Link;
-	//	Exp expression;
-
 
 	/**
 	 * Creates a new query object.
@@ -113,62 +101,4 @@ public class Query {
 	public void setEvents(ArrayList<String> e) {
 		this.events = e;
 	}
-
-//	public temp getTemporalLogic() {
-//		return temporalLogic;
-//	}
-//
-//	public void setTemporalLogic(temp temporalLogic) {
-//		this.temporalLogic = temporalLogic;
-//	}
-//
-//	public log getLogical() {
-//		return Logical;
-//	}
-//
-//	public void setLogical(log logical) {
-//		Logical = logical;
-//	}
-//
-//	public linkers getLink() {
-//		return Link;
-//	}
-//
-//	public void setLink(linkers link) {
-//		Link = link;
-//	}
-//
-//	public Exp getExpression() {
-//		return expression;
-//	}
-//
-//	public void setExpression(Exp expression) {
-//		this.expression = expression;
-//	}
-
-
-
-	//	public Query(Event e, temp t, linkers link, Exp exp){
-	//		this.e =e;
-	//		this.temporalLogic =t;
-	//		
-	//		this.Link =link;
-	//		this.expression = exp;
-	//	}
-	//	
-	//	public Query(Event e, log l, linkers link, Exp exp){
-	//		this.e =e;
-	//		
-	//		this.Logical =l;
-	//		this.Link =link;
-	//		this.expression = exp;
-	//	}
-	//	
-	//	public Query(Event e, temp t, log l, linkers link, Exp exp){
-	//		this.e =e;
-	//		this.temporalLogic =t;
-	//		this.Logical =l;
-	//		this.Link =link;
-	//		this.expression = exp;
-	//	}
 }
